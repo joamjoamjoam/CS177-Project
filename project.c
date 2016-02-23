56ebae719566d19cf4731da75780020c52c812a5
joamjoamjoam <trentcalllan@yahoo.com>
2016-02-22 18:36:23 -0800

    committed

--- project.c
+++ project.c
@@ -127,7 +127,7 @@ void calcTargetSpeed(){
     //while(1){
         
         for (int i = 0; i < numOfCars; i++) {
-            targetSpeedForCar[i] = 4;(rand() % 5) + 2;
+            targetSpeedForCar[i] = uniform(2,6);
         }
         //hold(uniform(60,120));
     //}
